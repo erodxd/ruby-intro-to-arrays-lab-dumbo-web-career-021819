ea9d2ec58c579c72f6d12bcd9f12f63842ff03f1
github username <github email address>
2019-01-31 13:42:18 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  instantiate_new_array = []
+  instantiate_new_array
+end
+
+def array_with_two_elements
+  array_with_two_elements = [1, 2]
+  array_with_two_elements
+end
+
+def first_element(taylor_swift)
+  taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+  taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  taylor_swift[-1]
+end
+
+def first_element_with_array_methods(first_country)
+  first_country.first
+end
+
+def last_element_with_array_methods(last_country)
+  last_country.last
+end
+
+def length_of_array(length)
+  length.length
+end
